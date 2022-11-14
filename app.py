d372ef42d7f60a969c50bdd5c49f6dda27a172b5
Bhawna <bhawnasaini496@gmail.com>
2022-11-14 10:59:40 +0530

    checking exception

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+import sys
+import os
+
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def index():
-    return "Starting Insurance Premium Prediction Project"
+    try:
+        raise Exception("We are testing the custom exception")
+    except Exception as e:
+        insurance = InsuranceException(e,sys)
+        logging.info(insurance.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
 if __name__ == "__main__":
     app.run(debug = True)
